96bad790acf45fbacffcd7e5a56900c43a82d236
AlemEsv <cinver.espinoza.v@uni.pe>
2025-04-12 09:02:39 -0500

    Modificar clase Quiz dentro del archivo trivia.py

--- trivia-game-python/app/trivia.py
+++ trivia-game-python/app/trivia.py
@@ -6,10 +6,13 @@ class Question:
 
     def is_correct(self, answer):
         return self.correct_answer == answer
+
 class Quiz:
     def __init__(self):
         self.questions = []
         self.current_question_index = 0
+        self.correct_answers = 0
+        self.incorrect_answers = 0
 
     def add_question(self, question):
         self.questions.append(question)
@@ -20,6 +23,15 @@ class Quiz:
             self.current_question_index += 1
             return question
         return None
+    
+    def answer_question(self, question, answer):
+        if question.is_correct(answer):
+            self.correct_answers += 1
+            return True
+        else:
+            self.incorrect_answers += 1
+            return False
+
     def run_quiz(self):
         question = self.get_next_question()
         if question:
