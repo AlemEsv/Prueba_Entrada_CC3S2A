b16e2b3a71476afa27a27265ea5058b28682043e
AlemEsv <cinver.espinoza.v@uni.pe>
2025-04-14 21:37:51 -0500

    Cambios en el dockerfile, configuración de GitHub Actions

--- trivia-game-python/app/trivia.py
+++ trivia-game-python/app/trivia.py
@@ -57,33 +57,4 @@ def run_quiz():
     print(f"Respuestas correctas: {quiz.correct_answers}")
     print(f"Respuestas incorrectas: {quiz.incorrect_answers}")
 
-if (__name__ == "__main__"):
-    print("""
-████████╗██████╗ ██╗██╗   ██╗██╗ █████╗      ██████╗  █████╗ ███╗   ███╗███████╗
-╚══██╔══╝██╔══██╗██║██║   ██║██║██╔══██╗    ██╔════╝ ██╔══██╗████╗ ████║██╔════╝
-   ██║   ██████╔╝██║██║   ██║██║███████║    ██║  ███╗███████║██╔████╔██║█████╗  
-   ██║   ██╔══██╗██║╚██╗ ██╔╝██║██╔══██║    ██║   ██║██╔══██║██║╚██╔╝██║██╔══╝  
-   ██║   ██║  ██║██║ ╚████╔╝ ██║██║  ██║    ╚██████╔╝██║  ██║██║ ╚═╝ ██║███████╗
-   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═══╝  ╚═╝╚═╝  ╚═╝     ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝
-                                                                            
-                             ─────────────────────
-                             ¡Bienvenido al juego!
-                             ─────────────────────
-                             1. Empezar Juego
-                             2. Leaderboard
-                             3. Salir
-""")
-    while True:
-        opcion = input("Elije una opción: ")
-        if opcion == '1':
-            run_quiz()
-            exit()
-        elif opcion == '2':
-            print("Mirar al leaderboard.") 
-        elif opcion == '3':
-            print("\nGracias por jugar!")
-            exit()
-        else:
-            print("Opción invalida.")
-
 
